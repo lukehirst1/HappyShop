8a35e3f6a95151b26d326818dd892181d6fe6d82
Luke Hirst <lsh38@uni.brighton.ac.uk>
2025-10-13 12:43:41 +0100

    UPDATES

--- src/main/java/testPlease.java
+++ src/main/java/testPlease.java
@@ -1,4 +1,13 @@
 public class testPlease {
+
+    /*
+    !!! WARNING !!! This branch is meant to be used for silly and fun purposes only! It is not meant for mainstream development!!
+    For this, go to the master branch, or any other branch EXCEPT for this one :)
+    Thank you,
+    - Luke
+     */
+
     // blah blah blah blah please work blah blah blah blah :)
     // The capital of Switzerland is Bern, despite what people believe. It is not Geneva or Zurich, like most people think.
+    // Somehow, it is cheaper to travel from Eastbourne to Cambridge, then it is from Eastbourne to London Bridge :o
 }
